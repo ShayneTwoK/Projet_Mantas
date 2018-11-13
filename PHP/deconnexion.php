206c5fcef1e8d14a5575a1e8ed7a88ae1e608fc3
Guepo38 <37220294+Guepo38@users.noreply.github.com>
2018-11-13 16:57:31 +0100

    Add files via upload

--- PHP/deconnexion.php
+++ PHP/deconnexion.php
@@ -4,8 +4,4 @@ session_unset();
 session_destroy();
 header('Location: connexion.php');
 exit();
-// Eh beh trop fort, t'a déja fait déconnecté alors que le gars
-// a même pas pu accéder un un bouton déconnecté... va pas
-// trop vite et fini t'es pages déjà que ta voulu faire inscription
-// ET connexion alors que c'etait ma tache et que tu fait de la merde...
 ?>
